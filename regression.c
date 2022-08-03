0ea2213aed43ffaf8394fa8cde963a15de336371
orbea <orbea@riseup.net>
2022-08-03 11:49:45 -0700

    regression: Add DragonFlyBSD patch
    https://github.com/DragonFlyBSD/DPorts/blob/0075f3ed253bc15d71c6d46038895125cae08a08/devel/trio/files/patch-regression.c

--- regression.c
+++ regression.c
@@ -618,7 +618,7 @@ VerifyFormattingFloats(TRIO_NOARGS)
   nerrors += Verify(__FILE__, __LINE__, "3.141500e+03    ",
 		    "%-16e", 3141.5);
   nerrors += Verify(__FILE__, __LINE__, "03.142e+03",
-		    "%010.3e", 3141.5);
+		    "%010.3e", 3141.51);
 #if !defined(TRIO_COMPILER_ANCIENT)
   /* Long double */
   nerrors += Verify(__FILE__, __LINE__, "1.400000",
