eb5864aa52ad0516857dc3920f06db98b9afe9ba
orbea <orbea@riseup.net>
2022-08-06 11:26:10 -0700

    Improve test-build.sh

--- test-build.sh
+++ test-build.sh
@@ -5,34 +5,91 @@ set -euf
 srcdir="$(cd -- "${0%/*}/" && pwd -P)"
 
 cd -- "$srcdir"
-[ ! -d .git ] || git clean -xdf
+
+if [ -d .git ]; then
+  git clean -xdf
+else
+  rm -rf build
+fi
+
+check=
+
+while [ $# -gt 0 ]; do
+  check="${check} $1"
+  shift
+done
+
 autoreconf -fi
+
+compiler='cc clang gcc'
+
+eval "set -- $compiler"
+for cc do
+  confdir="build/configure/$cc"
+  mkdir -p -- "$confdir"
+  (
+    cd -- "$confdir"/
+    printf %s\\n '' "Configuring for $cc" ''
+    CC="$cc" "$srcdir"/configure --enable-doc
+  )
+done
 
 build () {
   test="${1:-default}"
-  for compiler in cc clang gcc; do
+  cflags="${2:-}"
+  rm -rf -- build/"$test"
+  mkdir -p -- build/"$test"
+  eval "set -- $compiler"
+  for cc do
+    [ "$cc" != 'clang' ] || ! check_asan "$test" || continue
     for standard in '' -std=c89 -std=c99 -std=c11; do
+      # TODO: Add -O3 and -Ofast
       for optimization in '' -O1 -O2 -O0 -Os -Og; do
         build_string="$(printf %s "$standard" | tr = _)"
-        dir="build/${test}-${compiler}${optimization}${build_string}"
+        dir="build/${test}/${cc}${optimization}${build_string}"
         rm -rf -- "$dir"
-        mkdir -p -- "$dir"
+        cp -r -- build/configure/"$cc" "$dir"
         (
-          build_flags="${standard} ${optimization}"
           cd -- "$dir"/
-          printf %s\\n \
-            '' "Building ${compiler} ${build_flags}: ${test}" ''
-          CC="$compiler" CFLAGS="${build_flags} ${_CFLAGS} ${2:-}" \
-          "$srcdir"/configure --enable-doc
-          make install DESTDIR="$srcdir/$dir"/install V=1
-          make check V=1
+          build_flags="${standard} ${optimization}"
+          all_flags="${build_flags} ${_CFLAGS} ${cflags}"
+          printf %s\\n '' "Building ${cc} ${build_flags}: ${test}" ''
+          make install V=1 CFLAGS="$all_flags" DESTDIR="$srcdir/$dir"/install
+          make check V=1 CFLAGS="$all_flags"
         )
       done
     done
   done
 }
 
-_CFLAGS='-Werror -Wall'
+check_asan () {
+  case "$1" in
+    *san ) return 0 ;;
+    * ) return 1 ;;
+  esac
+}
 
-build
-build 'ansi' '-ansi'
+###########
+## Tests ##
+###########
+
+_CFLAGS='-Werror -Wall'
+_CFLAGS_ASAN='-g -fno-omit-frame-pointer'
+
+if [ -z "${check}" ]; then
+  # TODO: Indeterminate non-reproducible failures with lsan
+  check="default ansi lto asan tsan ubsan"
+fi
+
+for i in $(printf %s "${check}"); do
+  case "$i" in
+    ansi ) build 'ansi' '-ansi' ;;
+    lto ) build 'lto' '-flto' ;;
+    asan ) build 'asan' "${_CFLAGS_ASAN} -fsanitize=address" ;;
+    lsan ) build 'lsan' "${_CFLAGS_ASAN} -fsanitize=leak" ;;
+    tsan ) build 'tsan' "${_CFLAGS_ASAN} -fsanitize=thread" ;;
+    ubsan ) build 'ubsan' "${_CFLAGS_ASAN} -fsanitize=undefined" ;;
+    default ) build ;;
+    * ) printf %s\\n "ERROR: Unknown '$i' test." >&2; exit 1 ;;
+  esac
+done
