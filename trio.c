ac31e2e996b4d84c234f9a9a16b767d1c485113a
orbea <orbea@riseup.net>
2022-08-07 09:33:27 -0700

    Fix the build with -xc++

--- trio.c
+++ trio.c
@@ -812,7 +812,7 @@ typedef struct {
 #if TRIO_FEATURE_USER_DEFINED
   /* For the user-defined specifier */
   union {
-    char namespace[MAX_USER_NAME];
+    char trio_namespace[MAX_USER_NAME];
     int handler;        /* if flags & FLAGS_USER_DEFINED_PARAMETER */
   } user_defined;
   char user_data[MAX_USER_DATA];
@@ -1876,7 +1876,7 @@ TRIO_ARGS4((type, format, offset, parameter),
 	int ch;
 
 	parameter->type = FORMAT_USER_DEFINED;
-	parameter->user_defined.namespace[0] = NIL;
+	parameter->user_defined.trio_namespace[0] = NIL;
 
 	while ((ch = format[offset]) != NIL)
 	  {
@@ -1907,7 +1907,7 @@ TRIO_ARGS4((type, format, offset, parameter),
 		max = (int)(&format[offset] - tmpformat);
 		if (max > MAX_USER_NAME)
 		  max = MAX_USER_NAME;
-		trio_copy_max(parameter->user_defined.namespace, max, tmpformat);
+		trio_copy_max(parameter->user_defined.trio_namespace, max, tmpformat);
 		tmpformat = (char *)&format[offset];
 	      }
 	  }
@@ -3888,8 +3888,8 @@ TRIO_ARGS3((data, format, parameters),
 	      }
 	    else
 	      {
-		/* Look up namespace */
-		def = TrioFindNamespace(parameters[i].user_defined.namespace, NULL);
+		/* Look up trio_namespace */
+		def = TrioFindNamespace(parameters[i].user_defined.trio_namespace, NULL);
 	      }
 	    if (def)
 	      {
